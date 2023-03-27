2544ade902c99ed0a3fce0046009c3baefaf5add
Alex110915 <124754541+Alex110915@users.noreply.github.com>
2023-03-27 00:09:48 -0600

    Update fc.js

--- js/fc.js
+++ js/fc.js
@@ -1,15 +1,14 @@
 const columnaA = [
-    "Nosotros debemos pensar que somos una de las hojas de un arbol, y el arbol es toda la humanidad. No podemos vivir los unos sin los otros, sin el árbol.     -Pau Casals";
-    "La vida es una obra teatral que no importa cuanto haya durado, sino lo bien que haya sido representada.   -Séneca";
-    "Estar preparado es importante, saber esperar lo es aún más, pero aprovechar el momento adecuado es la clave de la vida.  -Arthur Schnitzler";
-    "Recuerda que la felicidad no depende en quién eres o qué tienes; depende únicamente en lo que piensas.  -Dale Carnegie";
-    "Lo que doy, me lo doy. Lo que no doy me lo quito. Nada para mí que no sea para otros.  -Alejandro  Jodorowsky";
-    "No hay hombre tan cobarde a quien el amor no haga valiente y transforme en héroe.  -Platón."
-    "No puedo enseñar nada a nadie. Solo puedo hacerles pensar.  - Sócrates."
-    "Vivir es nacer a cada instante.   -Erich Fromm. "
-    "¿Quieres ser rico? Pues no te afanes en aumentar tus bienes sino en disminuir tu codicia.  - Epicuro."
-    "La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia delante.   -Kierkegaard."
-    "La buena conciencia es la mejor almohada para dormir.   -Sócrates."
+    "Nosotros debemos pensar que somos una de las hojas de un arbol, y el arbol es toda la humanidad. No podemos vivir los unos sin los otros, sin el árbol.     -Pau Casals",
+    "La vida es una obra teatral que no importa cuanto haya durado, sino lo bien que haya sido representada.   -Séneca",
+    "Estar preparado es importante, saber esperar lo es aún más, pero aprovechar el momento adecuado es la clave de la vida.  -Arthur Schnitzler",
+    "Recuerda que la felicidad no depende en quién eres o qué tienes; depende únicamente en lo que piensas.  -Dale Carnegie",
+    "Lo que doy, me lo doy. Lo que no doy me lo quito. Nada para mí que no sea para otros.  -Alejandro  Jodorowsky",
+    "No hay hombre tan cobarde a quien el amor no haga valiente y transforme en héroe.  -Platón.",
+    "No puedo enseñar nada a nadie. Solo puedo hacerles pensar.  - Sócrates.",
+    "Vivir es nacer a cada instante.   -Erich Fromm. ",
+    "¿Quieres ser rico? Pues no te afanes en aumentar tus bienes sino en disminuir tu codicia.  - Epicuro.",
+    "La buena conciencia es la mejor almohada para dormir.   -Sócrates.",
 
 ]; 
 
